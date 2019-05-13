06dcfba6f209a7b40a5ae0b1933e88bb05e70d1b
Anthony <a82moreau@gmail.com>
2019-05-13 20:25:07 +0200

    mon_cv

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let competence = document.querySelector('#comp')
+let about_me = document.querySelector('#about')
+let objectif = document.querySelector('#obj')
+let portfolio = document.querySelector('#folio')
+let formation = document.querySelector('#form')
+let experience = document.querySelector('#exp')
+let contact = document.querySelector('#cont')
+
+
+
+let window_scroll = window.scrollY
+let scroll = competence.scrollHeight
+
+
+let link_active = document.querySelectorAll('nav .nav-link')
+
+
+
+console.log(window_scroll)
+
+
+
+
